613973a98148fc3ebf11706bfaba03f41fa1d991
MoustaphaGh30 <mmg30@mail.aub.edu>
2024-08-24 12:41:00 +0300

    first commit

---
+++ src/main/java/com/example/test_buddy/TestBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBuddyApplication.class, args);
+    }
+
+}
